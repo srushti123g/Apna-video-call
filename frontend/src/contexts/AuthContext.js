20f99e28a56adaea3b073325c22b513e0472ff9c
srushti123g <srushtigadakh441@gmail.com>
2025-06-23 13:08:16 +0530

    Deploy Frontend

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -4,13 +4,13 @@ import axios from "axios";
 import httpStatus from "http-status";
 import { createContext, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
-//import server from "../environment";
+import server from "../environment";
 
 
 export const AuthContext = createContext({});
 
 const client = axios.create({
-    baseURL: "http://localhost:3000/api/v1/users"
+    baseURL: `${server}/api/v1/users`
 })
 
 
---
+++ frontend/src/environment.js
@@ -0,0 +1,7 @@
+let IS_PROD=true;
+const server=IS_PROD ? 
+"https://apna-video-call-rn17.onrender.com":
+    "http://localhost:3000"
+    
+
+export default server;
